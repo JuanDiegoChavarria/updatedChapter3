e44426ae170a5314bfe9cb193a96d9da11fcbf3d
98h7 <98h7@fresnou.org>
2023-03-14 13:56:00 -0700

    initial commit

---
+++ functionNotes.js
@@ -0,0 +1,42 @@
+//function notes
+//step 1 - define the function
+// function sayHello(){
+//     console.log("Hello");
+// }
+//step2 - call (use) the function
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+// function printMax(){
+//     let x = 6;
+//     let y = 29;
+//     if(x>y){
+//         console.log(`the larger number is ${x}`)
+//     }else {
+//         console.log(`the larger number is ${y}`)
+//     }
+// }
+// printMax();
+
+// function printMax(x, y){
+//     if(x>y){
+//         console.log(`the larger number is ${x}`)
+//     }else {
+//         console.log(`the larger number is ${y}`)
+//     }
+// }
+//
+//
+// printMax(8,9);
+
+
+
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`)
+}
+
+sum(7,12);
+sum(0,5);
