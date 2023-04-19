717b268b80387b9c0076b7d3f7650e6a080a21e3
98h7 <98h7@fresnou.org>
2023-04-19 14:18:18 -0700

    charactercreator

--- charCreator.js
+++ charCreator.js
@@ -1,6 +1,10 @@
-const readLine = require('readline-sync')
+const readLine = require('readline-sync');
 
 let diceRolled = []
+let eyeColors = ['blue','green','brown','hazel','black'];
+let race = ['werewolf','vampire','','giant','dwarf','elf'];
+let job = ['Black Smith','stone mason','miner','swords men','king','prince']
+
 let character1 = {
 //empty to start
 }
@@ -13,7 +17,7 @@ function rolld6(){
 
 function rollDice(amount, list){
     for(let i=0; i<amount; i++){
-        let roll= rolld6();
+        let roll = rolld6();
         console.log(`You rolled a ${roll}!`)
         list.push(roll)
     }
@@ -33,9 +37,30 @@ function rollStat(list){
     rollDice(4, list);
     return sum(list)
 }
+function pickRandomChoice(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum]
+}
+function pickRandomChoice2(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum]
+}
+function pickRandomChoice3(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum]
+}
+function pickRandomChoice4(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum]
+}
+
 
 function createChar(list, character){
     character.name = readLine.question("Enter Character Name: ");
+    character.race = pickRandomChoice2(race);
+    character.eyeColor = pickRandomChoice(eyeColors);
+    character.job = pickRandomChoice3(job);
+
     console.log('Rolling STR')
     character.strength= rollStat(diceRolled);
     console.log('Rolling DEX')
@@ -48,15 +73,22 @@ function createChar(list, character){
     character.wisdom= rollStat(diceRolled);
     console.log('Rolling CHA')
     character.charisma= rollStat(diceRolled);
-    console.log(character)
+    character.luck = rolld6();
+    displayChar(character)
 }
 
 function displayChar(char){
     console.log(`Name: ${char.name}`);
+    console.log(`Race: ${char.race}`)
+    console.log(`Job: ${char.job}`)
+    console.log(`Eye color: ${char.eyeColor}`)
     console.log(`STR: ${char.strength}`);
     console.log(`DEX: ${char.dexterity}`);
     console.log(`CON: ${char.constitution}`);
     console.log(`INT: ${char.intelligence}`);
     console.log(`WIS: ${char.wisdom}`);
     console.log(`CHA: ${char.charisma}`);
+    console.log(`LUCK: ${char.luck}`)
+
 }
+console.log('---------------------------------')
