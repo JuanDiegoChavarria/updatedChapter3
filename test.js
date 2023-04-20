3e19f05fdf87a2ada1425817a66128890dd51d16
98h7 <98h7@fresnou.org>
2023-04-20 14:19:45 -0700

    character Creator

---
+++ test.js
@@ -0,0 +1,7 @@
+const readline=require('readline-sync');
+
+let mylist = [];
+mylist.push(readline.question('enter plural noun: '));
+let mylist2 = [];
+mylist.push(readline.question('enter plural noun: '));
+console.log(`Hello, my fellow ${mylist} the first in 2020, it's me, George Washington, ${mylist2} it's the first OCCUPATION. I am writing from (the) A PLACE. \nI have been secretly living for the past NUMBER years. I am concerned by the ADJECTIVE state of affairs in America these days.\n  I am in a state of affairs in America these days. Your politicians are VERB ENDING IN "ING PLURAL NOUN one another than with listening to the PLURAL NOUN of the people. \nWhen we declared our independence the A PLACE We set forth on a ADJECTIVE guided by the voices of the everyday PLURAL NOUN. If we are going to keep PLURAL NOUN ADJECTIVE-Path If we're going to VERB ENDING ING then we need to learn how to respect all PLURAL NOUN. \nDon't get me wrong; we had ADJECTIVEP problems in my day, too. Benjamin Franklin once called me a/an NOUN and kicked me in the VERB PART OF THE BODY But at the end of the day, we VERB in harmony. \nLet us find that ADJECTIVE spirit once again, or else I'm taking my PART OF THE BODY ADFECTIVE off the quarter! `)
