65b65697fe3072651327a11df63f443cd77fcafa
98h7 <98h7@fresnou.org>
2023-04-28 14:19:07 -0700

    combomenu

--- madlips.js
+++ madlips.js
@@ -1,25 +1,26 @@
 const readline=require('readline-sync');
 
 let mylist = [];
-mylist.push(readline.question('enter plural noun: '));
-mylist.push(readline.question('enter occupation : '));
+mylist.push(readline.question('enter a plural noun: '));
+mylist.push(readline.question('enter an occupation: '));
 mylist.push(readline.question('enter a place : '));
-mylist.push(readline.question('enter Number : '));
-mylist.push(readline.question('enter adjective : '));
-mylist.push(readline.question('enter verb ending in "ing" : '));
+mylist.push(readline.question('enter a number : '));
+mylist.push(readline.question('enter an adjective : '));
+mylist.push(readline.question('enter a verb ending in "ing" : '));
 mylist.push(readline.question('enter a plural noun : '));
 mylist.push(readline.question('enter a place : '));
-mylist.push(readline.question('enter a adjective : '));
+mylist.push(readline.question('enter an adjective : '));
 mylist.push(readline.question('enter a plural noun : '));
-mylist.push(readline.question('enter verb ending in "ing" : '));
+mylist.push(readline.question('enter a verb ending in "ing" : '));
 mylist.push(readline.question('enter a plural noun : '));
-mylist.push(readline.question('enter a adjective : '));
-mylist.push(readline.question('enter a noun : '));
+mylist.push(readline.question('enter an adjective : '));
+mylist.push(readline.question('enter a noun: '));
 mylist.push(readline.question('enter a part of the body: '));
 mylist.push(readline.question('enter a verb: '));
-mylist.push(readline.question('enter a adjective : '));
+mylist.push(readline.question('enter an adjective : '));
 mylist.push(readline.question('enter a part of the body: '));
+mylist.push(readline.question('enter an adjective : '));
+mylist.push(readline.question('enter a noun: '));
+mylist.push(readline.question('enter the name of a peron: '));
 
-
-console.log(`Hello, my fellow ${mylist[0]} the first in 2020, it's me, George Washington, it's the first OCCUPATION. I am writing from (the) A PLACE. \nI have been secretly living for the past NUMBER years. I am concerned by the ADJECTIVE state of affairs in America these days.\n  I am in a state of affairs in America these days. Your politicians are VERB ENDING IN "ING PLURAL NOUN one another than with listening to the PLURAL NOUN of the people. \nWhen we declared our independence the A PLACE We set forth on a ADJECTIVE guided by the voices of the everyday PLURAL NOUN. If we are going to keep PLURAL NOUN ADJECTIVE-Path If we're going to VERB ENDING ING then we need to learn how to respect all PLURAL NOUN. \nDon't get me wrong; we had ADJECTIVEP problems in my day, too. Benjamin Franklin once called me a/an NOUN and kicked me in the VERB PART OF THE BODY But at the end of the day, we VERB in harmony. \nLet us find that ADJECTIVE spirit once again, or else I'm taking my PART OF THE BODY ADFECTIVE off the quarter! `)
-//
+console.log(`Hello, my fellow${mylist[0]} in 2023,it's me, George Washington, the first${mylist[1]}.I am writing from ${mylist[2]}, where I have been secretly living for the past ${mylist[3]} years. I am concerned by the ${mylist[4]} state of affairs in America these days.\nit seems that your politicians are more with ${mylist[5]} one another than with listening to the${mylist[6]} of the people. \nWhen we declared our independence the ${mylist[7]}, we set forth on a ${mylist[8]} guided by the voices of the everyday ${mylist[9]}.\nIf we're going to ${mylist[10]}then we need to learn how to respect all ${mylist[11]}. \nDon't get me wrong; we had${mylist[12]} problems in my day, too.\nBenjamin Franklin once called me a/an ${mylist[13]} and kicked me in the ${mylist[14]} But at the end of the day, we ${mylist[15]}in harmony. \nLet us find that ${mylist[16]} spirit once again, or else I'm taking my ${mylist[17]} off the quarter! By the way tell Trump to make america ${mylist[18]} again,${mylist[19]} prices have more than doubled and people dont want ${mylist[20]}to be president again`)
