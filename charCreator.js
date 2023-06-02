34fcbce0874cf186862cb423768c10a7d5cf28e3
98h7 <98h7@fresnou.org>
2023-06-02 14:19:08 -0700

    combo menu

--- charCreator.js
+++ charCreator.js
@@ -1,19 +1,16 @@
-const readLine = require ('readline-sync');
 
-let diceRolled = [];
+
+const readline = require('readline-sync');
+
+let diceRolled = []
+let eyeColors = ['blue','green','brown','hazel','black'];
+let Race = ['Human','Fairy','Goblin','Goliath','Mexican','Asian','Dwarf','Monkey','Giant','God','Angel','Devil','Demon','American'];
+let Ability = ['super strenth', ' super vision', 'flying ability']
 let character1 = {
-}
-
-let character2 = {
-}
-
-let character3 = {
+//empty to start
 }
 
 createChar(character1, diceRolled);
-createChar(character2, diceRolled);
-createChar(character3, diceRolled);
-
 function rolld6(){
     return Math.ceil(Math.random()*6)
 }
@@ -21,60 +18,68 @@ function rolld6(){
 function rollDice(amount, list){
     for (let i=0; i<amount; i++){
         let roll = rolld6();
-        console.log(`You rolled ${roll}!`)
+        console.log(`You rolled a ${roll}!`);
         list.push(roll);
     }
 }
 function sum(list){
-    let sum = 0;
+    let sum=0;
     for (let i=0; i<list.length; i++){
-        sum += list[i]
+        sum+=list[i]
     }
+    console.log(`The total was: ${sum}.`)
     return sum;
 }
+
 function rollStat(list){
-    list = []
-    rollDice(4, list);
+    list = [];
+    rollDice(6, list);
     return sum(list);
 }
+
+function pickRandomChoice(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum];
+}
+
+function pickRandomChoice2(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum];
+}
 function createChar(character, list){
-    character.name = readLine.question("Enter Character Name: ")
-    character.age = readLine.question("Enter Character Age: ")
-    console.log("Rolling STR")
+    character.name = readline.question("Enter Character Name: ");
+    character.eyeColor = pickRandomChoice(eyeColors);
+    character.Race = pickRandomChoice2(Race);
+    console.log("Rolling Strength")
     character.strength = rollStat(diceRolled);
-    console.log("Rolling DEX")
-    character.dexerity = rollStat(diceRolled);
-    console.log("Rolling CON")
-    character.constitution = rollStat(diceRolled);
-    console.log("Rolling INT")
+    console.log("Rolling Speed");
+    character.speed = rollStat(diceRolled);
+    console.log("Rolling Magic")
+    character.magic = rollStat(diceRolled);
+    console.log("Rolling Skill")
+    character.skill = rollStat(diceRolled);
+    console.log("Rolling Intelligence")
     character.intelligence = rollStat(diceRolled);
-    console.log("Rolling WIS")
+    console.log("Rolling Wisdom")
     character.wisdom = rollStat(diceRolled);
-    console.log("Rolling CHA")
-    character.charisma = rollStat(diceRolled);
-    console.log("Rolling Cooking Skills")
-    character.cook = rollStat(diceRolled);
-    console.log("Rolling Social Skills")
-    character.sk = rollStat(diceRolled);
-    console.log("Rolling HP")
-    character.hp = rollStat(diceRolled);
-    displayChar(character1)
-    displayChar(character2)
-    displayChar(character3)
+    console.log("Rolling Durability")
+    character.durability = rollStat(diceRolled);
+    character.luck = rolld6();
+    console.log(character)
 }
 
 function displayChar(char){
-    console.log('------------------------')
+    console.log('-------------------------');
     console.log(`Name: ${char.name}`);
-    console.log(`Age: ${char.age}`)
+    console.log(`Race: ${char.race}`);
+    console.log(`Eye Color: ${char.eyeColor}`);
     console.log(`STR: ${char.strength}`);
-    console.log(`DEX: ${char.dexerity}`);
-    console.log(`CON: ${char.constitution}`);
+    console.log(`SPE: ${char.speed}`);
+    console.log(`DEX: ${char.magic}`);
+    console.log(`CON: ${char.skill}`);
     console.log(`INT: ${char.intelligence}`);
     console.log(`WIS: ${char.wisdom}`);
-    console.log(`CHA: ${char.charisma}`);
-    console.log(`COOKING: ${char.cook}`);
-    console.log(`SOCIAL SKILLS: ${char.sk}`);
-    console.log(`HP: ${char.hp}`);
-    console.log('------------------------')
+    console.log(`DUR: ${char.durability}`);
+    console.log(`LUCK: ${char.luck}`);
+    console.log('-------------------------------');
 }
